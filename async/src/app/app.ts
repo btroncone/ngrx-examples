7a730b09b0e1afc4ffa541710679675eacbcf7f9
Brian Troncone <btroncone@gmail.com>
2016-02-12 21:42:51 -0500

    added missed file

---
+++ async/src/app/app.ts
@@ -0,0 +1,21 @@
+import { Component } from 'angular2/core';
+import {Counter} from './components/counter';
+
+@Component({
+	selector: `app`,
+	template: `
+<div id="layout" class="pure-g">
+    <div class="sidebar pure-u-1 pure-u-md-1-4">
+        <div class="header">
+            <h1 class="brand-title">NgRx Store</h1>
+            <h2 class="brand-tagline">Example #3 - Async</h2>
+        </div>
+    </div>
+    <div class="content pure-u-1 pure-u-md-3-4">
+        <counter></counter>
+    </div>
+</div>
+	`,
+    directives: [Counter]
+})
+export class App {}

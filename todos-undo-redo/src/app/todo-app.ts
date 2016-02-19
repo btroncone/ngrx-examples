ae6e30ca639ada54261f17030de613546e4d7591
Brian Troncone <btroncone@gmail.com>
2016-02-18 22:01:42 -0500

    updated package.json

--- todos-undo-redo/src/app/todo-app.ts
+++ todos-undo-redo/src/app/todo-app.ts
@@ -11,7 +11,7 @@ import {TodosActions} from "./actions/todos.actions";
 		<div class="sidebar pure-u-1 pure-u-md-1-4">
 			<div class="header">
 				<h1 class="brand-title">NgRx Store</h1>
-				<h2 class="brand-tagline">Example #2 - Todos</h2>
+				<h2 class="brand-tagline">Example #4 - Todos Undo/Redo</h2>
 			</div>
 		</div>
 		<div class="content pure-u-1 pure-u-md-3-4">

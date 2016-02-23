d9b24253f3a3a4a5e99e56d1ad22ac143a0ec4e2
Brian Troncone <btroncone@gmail.com>
2016-02-22 21:45:30 -0500

    update counter to beta.7

--- counter/src/polyfills.ts
+++ counter/src/polyfills.ts
@@ -2,9 +2,10 @@
 import 'es6-shim';
 // (these modules are what are in 'angular2/bundles/angular2-polyfills' so don't use that here)
 import 'es6-promise';
-import 'rxjs';
 
 if ('production' === process.env.ENV) {
+    // Production
+
     // In production Reflect with es7-reflect-metadata/reflect-metadata is added
 
     // Zone.js
@@ -13,13 +14,26 @@ if ('production' === process.env.ENV) {
     // RxJS
     // In production manually include the operators you use
     require('rxjs/add/operator/map');
+    require('rxjs/add/operator/mergeMap');
 
 } else {
+    // Development
+
     // Reflect Polyfill
     require('es7-reflect-metadata/src/global/browser');
     // In production Reflect with es7-reflect-metadata/reflect-metadata is added
+
     // by webpack.prod.config ProvidePlugin
     Error['stackTraceLimit'] = Infinity;
     require('zone.js/dist/zone-microtask');
     require('zone.js/dist/long-stack-trace-zone');
+
+    // RxJS
+    // In development we are including every operator
+    require('rxjs/add/operator/map');
+    require('rxjs/add/operator/mergeMap');
+
 }
+
+// For vendors for example jQuery, Lodash, angular2-jwt just import them anywhere in your app
+// Also see custom_typings.d.ts as you also need to do `typings install x` where `x` is your module
